e367f3487e445134aedc2914859a29ecbdad2c0e
ssoni09 <swatisoni984@gmail.com>
2025-01-10 21:56:35 +0530

    Sign out logic

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -3,8 +3,14 @@ import Header from './Header'
 import { checkValidData } from '../utils/validate';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const [isSignInForm , setIsSignInForm] = useState(true);
   const [errorMessage , setErrorMessage] = useState(true);
@@ -33,6 +39,26 @@ const Login = () => {
       )
         .then((userCredential) => {
           const user = userCredential.user;
+          updateProfile(auth.currentUser, {
+            displayName: name.current.value, 
+            photoURL: "https://avatars.githubusercontent.com/u/108583285?v=4"
+          }).then(() => {
+            // Profile updated!
+            const { uid, email, displayName, photoURL } = auth.currentUser;
+            dispatch(
+              addUser({
+                uid: uid,
+                email: email,
+                displayName: displayName,
+                photoURL: photoURL,
+              })
+            );
+            navigate('/browse');
+          }).catch((error) => {
+            // An error occurred
+            setErrorMessage(error.message);
+          });
+          
           // console.log(user);
         })
         .catch((error) => {
@@ -50,6 +76,7 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
+          navigate('/browse');
           // console.log(user);
         })
         .catch((error) => {
